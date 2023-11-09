b00f8f7124489a0dc606d62724cc2cabb0e22645
Naveenn789 <naveengalla77@gmail.com>
2023-11-09 16:13:42 +0530

    intial commit

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramBasicPart5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter your score as a percentage");
+            int X = int.Parse(Console.ReadLine());
+            if(X >= 90)
+            {
+                Console.WriteLine("you got Grade " + 'A' + " "+X);
+            }
+            else if (X >= 80 && X<=89)
+            {
+                Console.WriteLine("you got Grade " + 'B' + " " + X);
+            }
+            else if (X >= 70 && X <= 79)
+            {
+                Console.WriteLine("you got Grade " + 'C' + " " + X);
+            }
+            else if (X >= 60 && X <= 69)
+            {
+                Console.WriteLine("you got Grade " + 'D' + " " + X);
+            }
+            else
+            {
+                Console.WriteLine("You got Grade "+'F' + " " + X);
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
